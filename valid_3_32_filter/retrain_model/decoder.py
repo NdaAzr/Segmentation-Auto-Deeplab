54aa55fb49c7671ca1981f5f4beb9a950e49b51f
NdAI <35597803+NdaAzr@users.noreply.github.com>
2020-12-23 13:23:24 +0000

    added

---
+++ valid_3_32_filter/retrain_model/decoder.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+#from operations_2 import NaiveBN
+
+
+class Decoder(nn.Module):
+    def __init__(self, num_classes, filter_multiplier, args=None, last_level=0): #BatchNorm
+        super(Decoder, self).__init__()
+        low_level_inplanes = filter_multiplier
+        print(low_level_inplanes)
+        C_low = 48
+        self.conv1 = nn.Conv2d(low_level_inplanes, C_low, 1, bias=False)
+        self.bn1 = nn.BatchNorm2d(48)
+        self.last_conv = nn.Sequential(nn.Conv2d(304,256, kernel_size=3, stride=1, padding=1, bias=False),#304
+                                       nn.BatchNorm2d(256),
+                                       nn.Dropout(0.5),
+                                       nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=False),
+                                       nn.BatchNorm2d(256),
+                                       nn.Dropout(0.1),
+                                       nn.Conv2d(256, num_classes, kernel_size=1, stride=1))
+        self._init_weight()
+
+    def forward(self, x, low_level_feat):
+        low_level_feat = self.conv1(low_level_feat)
+        low_level_feat = self.bn1(low_level_feat)
+
+        x = F.interpolate(x, size=low_level_feat.size()[2:], mode='bilinear', align_corners=True)
+        x = torch.cat((x, low_level_feat), dim=1)
+        x = self.last_conv(x)
+        return x
+
+    def _init_weight(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                torch.nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
